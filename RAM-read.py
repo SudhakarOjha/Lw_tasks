d80836bb23977df52973cee6c52ba787a0aa6003
Sudhakar Ojha <146667332+SudhakarOjha@users.noreply.github.com>
2025-07-21 01:02:21 +0530

    Add files via upload

---
+++ RAM-read.py
@@ -0,0 +1,9 @@
+import psutil
+
+mem = psutil.virtual_memory()
+
+
+print(f"Total RAM: {mem.total / (1024 ** 3):.2f} GB")
+print(f"Available RAM: {mem.available / (1024 ** 3):.2f} GB")
+print(f"Used RAM: {mem.used / (1024 ** 3):.2f} GB")
+print(f"RAM Usage Percent: {mem.percent}%")
